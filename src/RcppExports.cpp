06abd3085bec05bfbfe55dd74e1453118435727f
Ayelet Peres <aperes7@gmail.com>
2025-01-27 13:20:00 -0500

    update allele diff

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,33 +11,82 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // allele_diff_strings
-std::vector<std::string> allele_diff_strings(std::vector<std::string> germs, int X);
-RcppExport SEXP _piglet_allele_diff_strings(SEXP germsSEXP, SEXP XSEXP) {
+std::vector<std::string> allele_diff_strings(std::vector<std::string> germs, int X, Rcpp::Nullable<Rcpp::CharacterVector> non_mismatch_chars_nullable);
+RcppExport SEXP _piglet_allele_diff_strings(SEXP germsSEXP, SEXP XSEXP, SEXP non_mismatch_chars_nullableSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< std::vector<std::string> >::type germs(germsSEXP);
     Rcpp::traits::input_parameter< int >::type X(XSEXP);
-    rcpp_result_gen = Rcpp::wrap(allele_diff_strings(germs, X));
+    Rcpp::traits::input_parameter< Rcpp::Nullable<Rcpp::CharacterVector> >::type non_mismatch_chars_nullable(non_mismatch_chars_nullableSEXP);
+    rcpp_result_gen = Rcpp::wrap(allele_diff_strings(germs, X, non_mismatch_chars_nullable));
     return rcpp_result_gen;
 END_RCPP
 }
 // allele_diff_indices
-std::vector<int> allele_diff_indices(std::vector<std::string> germs, int X);
-RcppExport SEXP _piglet_allele_diff_indices(SEXP germsSEXP, SEXP XSEXP) {
+std::vector<int> allele_diff_indices(std::vector<std::string> germs, int X, Rcpp::Nullable<Rcpp::CharacterVector> non_mismatch_chars_nullable);
+RcppExport SEXP _piglet_allele_diff_indices(SEXP germsSEXP, SEXP XSEXP, SEXP non_mismatch_chars_nullableSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< std::vector<std::string> >::type germs(germsSEXP);
     Rcpp::traits::input_parameter< int >::type X(XSEXP);
-    rcpp_result_gen = Rcpp::wrap(allele_diff_indices(germs, X));
+    Rcpp::traits::input_parameter< Rcpp::Nullable<Rcpp::CharacterVector> >::type non_mismatch_chars_nullable(non_mismatch_chars_nullableSEXP);
+    rcpp_result_gen = Rcpp::wrap(allele_diff_indices(germs, X, non_mismatch_chars_nullable));
+    return rcpp_result_gen;
+END_RCPP
+}
+// allele_diff_indices_parallel
+Rcpp::RObject allele_diff_indices_parallel(std::vector<std::string> germs, std::vector<std::string> inputs, int X, bool parallel, bool return_count);
+RcppExport SEXP _piglet_allele_diff_indices_parallel(SEXP germsSEXP, SEXP inputsSEXP, SEXP XSEXP, SEXP parallelSEXP, SEXP return_countSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type germs(germsSEXP);
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type inputs(inputsSEXP);
+    Rcpp::traits::input_parameter< int >::type X(XSEXP);
+    Rcpp::traits::input_parameter< bool >::type parallel(parallelSEXP);
+    Rcpp::traits::input_parameter< bool >::type return_count(return_countSEXP);
+    rcpp_result_gen = Rcpp::wrap(allele_diff_indices_parallel(germs, inputs, X, parallel, return_count));
+    return rcpp_result_gen;
+END_RCPP
+}
+// allele_diff_indices_parallel2
+Rcpp::RObject allele_diff_indices_parallel2(std::vector<std::string> germs, std::vector<std::string> inputs, int X, bool parallel, bool return_count, Rcpp::Nullable<Rcpp::CharacterVector> non_mismatch_chars_nullable);
+RcppExport SEXP _piglet_allele_diff_indices_parallel2(SEXP germsSEXP, SEXP inputsSEXP, SEXP XSEXP, SEXP parallelSEXP, SEXP return_countSEXP, SEXP non_mismatch_chars_nullableSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type germs(germsSEXP);
+    Rcpp::traits::input_parameter< std::vector<std::string> >::type inputs(inputsSEXP);
+    Rcpp::traits::input_parameter< int >::type X(XSEXP);
+    Rcpp::traits::input_parameter< bool >::type parallel(parallelSEXP);
+    Rcpp::traits::input_parameter< bool >::type return_count(return_countSEXP);
+    Rcpp::traits::input_parameter< Rcpp::Nullable<Rcpp::CharacterVector> >::type non_mismatch_chars_nullable(non_mismatch_chars_nullableSEXP);
+    rcpp_result_gen = Rcpp::wrap(allele_diff_indices_parallel2(germs, inputs, X, parallel, return_count, non_mismatch_chars_nullable));
+    return rcpp_result_gen;
+END_RCPP
+}
+// insert_gaps2_vec
+std::vector<std::string> insert_gaps2_vec(const std::vector<std::string>& gapped, const std::vector<std::string>& ungapped, bool parallel);
+RcppExport SEXP _piglet_insert_gaps2_vec(SEXP gappedSEXP, SEXP ungappedSEXP, SEXP parallelSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const std::vector<std::string>& >::type gapped(gappedSEXP);
+    Rcpp::traits::input_parameter< const std::vector<std::string>& >::type ungapped(ungappedSEXP);
+    Rcpp::traits::input_parameter< bool >::type parallel(parallelSEXP);
+    rcpp_result_gen = Rcpp::wrap(insert_gaps2_vec(gapped, ungapped, parallel));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_piglet_allele_diff_strings", (DL_FUNC) &_piglet_allele_diff_strings, 2},
-    {"_piglet_allele_diff_indices", (DL_FUNC) &_piglet_allele_diff_indices, 2},
+    {"_piglet_allele_diff_strings", (DL_FUNC) &_piglet_allele_diff_strings, 3},
+    {"_piglet_allele_diff_indices", (DL_FUNC) &_piglet_allele_diff_indices, 3},
+    {"_piglet_allele_diff_indices_parallel", (DL_FUNC) &_piglet_allele_diff_indices_parallel, 5},
+    {"_piglet_allele_diff_indices_parallel2", (DL_FUNC) &_piglet_allele_diff_indices_parallel2, 6},
+    {"_piglet_insert_gaps2_vec", (DL_FUNC) &_piglet_insert_gaps2_vec, 3},
     {NULL, NULL, 0}
 };
 
